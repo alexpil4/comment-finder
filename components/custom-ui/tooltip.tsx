3953c72f12e795e360f79554bedab2ca4f90b053
Alessandro Pilastrini <133789773+apilastrini@users.noreply.github.com>
2025-03-21 10:11:09 +0100

    improved tooltip

--- components/custom-ui/tooltip.tsx
+++ components/custom-ui/tooltip.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client';
+
+import clsx from 'clsx';
 import { XIcon } from 'lucide-react';
 
 interface Props {
@@ -10,8 +12,15 @@ interface Props {
 }
 
 export default function Tooltip({ children, text, isVisible, arrowPosition, onClose }: Props) {
+  
+  const setArrowPosition = clsx({
+    'left-5': arrowPosition === 'left',
+    'right-5': arrowPosition === 'right',
+    'left-1/2': arrowPosition === 'center',
+  });
+
   return (
-    <div className="flex flex-grow flex-col items-center">
+    <div className="flex-grow flex flex-col items-center">
       {children}
       {isVisible && (
         <div className="absolute min-w-[290px] top-20 p-2 bg-yellow-300 rounded-md text-sm shadow-md">
@@ -23,9 +32,7 @@ export default function Tooltip({ children, text, isVisible, arrowPosition, onCl
             strokeWidth={3}
           />
           <div
-            className={`absolute ${
-              arrowPosition === 'center' ? 'left-1/2' : `${arrowPosition}-5`
-            } bottom-full border-8 border-x-transparent border-t-transparent border-b-yellow-300`}
+            className={`absolute ${setArrowPosition} bottom-full border-8 border-x-transparent border-t-transparent border-b-yellow-300`}
           ></div>
         </div>
       )}
