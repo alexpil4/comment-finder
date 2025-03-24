4acf490ccce58b8c7d5a54b8f1d444611e466cee
Alessandro Pilastrini <133789773+apilastrini@users.noreply.github.com>
2025-03-24 01:10:26 +0100

    Avatar resized

--- utils/randomAvatarIDGenerator.ts
+++ utils/randomAvatarIDGenerator.ts
@@ -1,3 +1,3 @@
-export function getAvatarRandomID(min: number = 1, max: number = 20): number {
+export function getAvatarRandomID(min: number = 1, max: number = 10): number {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
